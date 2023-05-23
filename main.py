ae71053e6e3600e83d85af68cc2082d910195bbd
flooooooooooooooo <avenel.florio@gmail.com>
2023-05-23 10:41:34 +0200

    N_x et N_t

--- main.py
+++ main.py
@@ -32,12 +32,12 @@ def open_input_file():
 
 def initialize_data_numerical_solving(t_fin, N_t, L, N_x, C_0, x_d, x_f, D):
     """Initialise les données pour la résolution du schéma numérique"""
-    dt = t_fin / (N_t + 1)
-    dx = L / (N_x + 1)
+    dt = t_fin / N_t
+    dx = L / N_x
+    print(dx, dx * N_x, L)
     x = 0
     t = 0
     C = np.zeros((N_x,N_t))
-    print(C.shape)
     R = D * dt / (dx ** 2)
 
     for i in range(0,N_x):
@@ -64,7 +64,7 @@ def solve_concentration_numericaly(N_t, N_x, R, C,t_fin,dt):
             if j == 0:
                 """w = 10*math.pi/t_fin
                 C[j,i+1] = 1 + 1*math.sin(w*t)""" # potentiel fonction
-                C[j,i+1] = 1 # potentiel fonction
+                C[j,i+1] = 0 # potentiel fonction
             elif j == N_x -1:
                 C[j,i+1] = 0 # potentiel fonction
             else:
@@ -105,8 +105,8 @@ def plot_concentration(C, N_t):
 
 def plot_numerical_exact_comparison(C_verif, C):
     """Plot la comparaison entre la solution exacte et la solution numérique"""
-    plt.plot(C_verif[:,N_t-1])
-    plt.plot(C[:,N_t-1])
+    plt.plot(C_verif[:,N_t-1], color="blue")
+    plt.plot(C[:,N_t-1], color="red")
     plt.savefig("output/numerical_exact_comparison.png")
     plt.clf()
 
@@ -114,7 +114,11 @@ def video_concentration():
     subprocess.call("ffmpeg -s 800x600 -i output/C_000%d.png -vcodec libx264 -crf 25 -pix_fmt yuv420p output/video_concentration.mp4", shell=True)
 
 def end_plot(C,N_t):
+
     plt.plot(C[:,N_t-1])
+    plt.title("Concentration en fonction de la position à t = 1000 s")
+    plt.xlabel("Position")
+    plt.ylabel("Concentration")
     plt.show()
 
 """Main"""
@@ -122,9 +126,10 @@ C_0, L, x_d, x_f, D, N_x, t_fin, N_t = open_input_file()
 dt, dx, x, t, C, R = initialize_data_numerical_solving(t_fin, N_t, L, N_x, C_0, x_d, x_f, D)
 C = solve_concentration_numericaly(N_t, N_x, R, C,t_fin,dt)
 C_verif = initialize_data_exact_solving(N_x)
-C_verif = solve_concentration_exactly(dx, dt, C_verif, N_t, N_x, D)
-diff = difference_exact_numerique(C_verif,C,N_t,N_x)
-plot_numerical_exact_comparison(C_verif, C)
-end_plot(diff,N_t)
+#C_verif = solve_concentration_exactly(dx, dt, C_verif, N_t, N_x, D)
+#diff = difference_exact_numerique(C_verif,C,N_t,N_x)
+initialize_output_file()
+#plot_numerical_exact_comparison(C_verif, C)
+end_plot(C,N_t)
 
 
